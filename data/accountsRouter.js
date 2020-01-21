c5f0957d59f9b9565040dd826b65a3ea1c7c6805
Schrese <53787633+Schrese@users.noreply.github.com>
2020-01-21 15:39:02 -0500

    get and post requests work

---
+++ data/accountsRouter.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const knex = require('./dbConfig.js')
+
+const router = express.Router();
+
+//returns a list of accounts
+router.get('/', (req, res) => {
+    knex.select('*').from('accounts')
+        .then(accounts => {
+            console.log(accounts)
+            res.status(200).json(accounts)
+        })
+        .catch(err => {
+            console.log('error getting accounts', err)
+            res.status(500).json({ errorMessage: 'Could not retrieve accounts' })
+        })
+})
+
+router.post('/', (req, res) => {
+    const accountData = req.body;
+
+    knex('accounts').insert(accountData, 'id')
+        .then(newAct => {
+            const id = newAct[0]
+            return knex('accounts').where({id}).first().select('id', 'name', 'budget').then(act => {
+                res.status(201).json(act)
+            })
+        })
+        .catch(err => {
+            console.log('error creating account', err)
+            res.status(500).json({ errorMessage: 'Could not create account' })
+        })
+})
+
+module.exports = router;
